bf944d5683f4fb728e6d40310db16844f965bcb9
MCRDR <dukkoong@gmail.com>
2023-03-17 16:12:23 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,62 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '남덕현',
-      home: RandomWords(),
+      home: SplashScreen(),
     );
   }
 }
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // 일정 시간이 지난 후에 홈 화면으로 이동
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => RandomWords()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/inha.png'),
+      ),
+    );
+  }
+}
+
+/*
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Splash Screen'),
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+    );
+  }
+}
+ */
 class RandomWordsState extends State<RandomWords>{
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
@@ -33,24 +73,24 @@ class RandomWordsState extends State<RandomWords>{
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-   });
+          final index = i ~/ 2;
+          if (index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 
   Widget _buildRow(WordPair pair){
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      )
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        )
     );
   }
 }
